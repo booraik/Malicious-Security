37e43ddef758e0db508dd20494abb0b11494a7c6
laphs <mail.laphs@gmail.com>
2020-12-17 17:43:06 +0900

    Add crontab script

---
+++ script/crontab_iptables.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $# -ge 1 ]; then
+  HOME=$1
+else
+  HOME="."
+fi
+cd $HOME
+
+# 1. Update latest version
+git --git-dir=$HOME/.git/ pull origin main
+
+# 2. update blacklist from local detection
+script/fail2ban_sshd_to_blacklist.sh
+script/ipset_to_blacklist.sh rdp blacklist_rdp
+
+# 3. commit && push
+git add blacklist_ip
+git commit -m  "[`date`] Commited by `hostname`"
+git push origin main
+
+# 4. apply
+script/blacklist_to_ipset.sh
+fail2ban-client unban --all

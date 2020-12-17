0674d5ee1ec74df147bc16c77d48e5a915e54167
laphs <mail.laphs@gmail.com>
2020-12-17 16:32:49 +0900

    Add ipset to blacklist

---
+++ script/ipset_to_blacklist.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "Usage: ipset_to_blacklist.sh <ssh | rdp | http> <ipset name>"
+  exit
+fi
+
+HOME="."
+cd $HOME
+
+TARGET=$1
+IPSET_NAME=$2
+
+cp blacklist_ip/$TARGET.txt .tmp
+echo $TARGET Blacklist IP address count: $(cat .tmp| wc -w)
+
+NEW_IP=$(ipset save $IPSET_NAME | grep -Eo "([0-9]{1,3}[\.]){3}[0-9]{1,3}")
+echo New Malicious IP address count: $(echo $NEW_IP | wc -w)
+
+printf "%s\n" "${NEW_IP[@]}" >> .tmp
+echo Added Blacklist IP address count: $(cat .tmp | wc -w)
+
+cat .tmp | sort -h | uniq > blacklist_ip/$TARGET.txt
+rm -f .tmp
+echo Uniq Blacklist IP address count: $(cat blacklist_ip/$TARGET.txt | wc -w)

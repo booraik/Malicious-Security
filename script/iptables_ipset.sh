d74e7879af54be982485f7d5727e33d007334454
laphs <mail.laphs@gmail.com>
2020-12-27 23:34:14 +0900

    Add ipset to iptables_ipset.sh script

--- script/iptables_ipset.sh
+++ script/iptables_ipset.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+IPSET=/usr/sbin/ipset
+$IPSET create blacklist_rdp hash:net hashsize 4096 2> /dev/null
+$IPSET create blacklist_ssh hash:net hashsize 4096 2> /dev/null
+$IPSET create blacklist_http hash:net hashsize 4096 2> /dev/null
+$IPSET create blacklist_rescure hash:net hashsize 4096 2> /dev/null
+
 iptables -N BLACKLIST 2> /dev/null
 iptables -F BLACKLIST 2> /dev/null
 iptables -A BLACKLIST -m set --match-set blacklist_ssh src -j REJECT --reject-with icmp-port-unreachable
